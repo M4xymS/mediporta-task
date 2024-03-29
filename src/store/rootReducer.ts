5b46a395dd87b725b7cc6594a91a1c8c10b83252
Maksym <smolinskimaksym@gmail.com>
2024-03-29 13:06:45 +0100

    added api to store and reducers

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,8 @@
 import {combineReducers} from "@reduxjs/toolkit";
+import {api} from "@/api/api.ts";
 
 const rootReducer = combineReducers({
-
+    [api.reducerPath]: api.reducer,
 })
 
 export default rootReducer
