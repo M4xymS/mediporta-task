48d75f4e73a2e587dfda39e7d448a1d9f4a27e3a
Maksym <smolinskimaksym@gmail.com>
2024-03-29 17:54:29 +0100

    added types to api

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+import {ReactNode} from "react";
+
+export interface Tag {
+    collectives?: Collective[]
+    has_synonyms: boolean
+    is_moderator_only: boolean
+    is_required: boolean
+    count: number
+    name: string
+}
+
+export type External_Link = {
+    type: string
+    link: string
+}
+
+export interface Collective {
+    tags: string[]
+    external_links: External_Link[]
+    description: string
+    link: string
+    name: string
+    slug: string
+}
+
+export interface APIResponse {
+    items: Tag[]
+    has_more: boolean;
+    quota_max: number
+    quota_remaining: number
+}
+
+export interface Column<T> {
+    key: keyof T,
+    id?: string,
+    size?: number,
+    header?: string | ReactNode;
+    valueFormatter?: (value: typeof T) => ReactNode;
+}
+
+export type ValueOf<T> = T[keyof T]
