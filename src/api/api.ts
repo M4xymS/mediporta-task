244de2ccf63f0e1c1bcea2dc9cd3f0ff66251349
Maksym <smolinskimaksym@gmail.com>
2024-03-29 13:06:38 +0100

    added redux api

---
+++ src/api/api.ts
@@ -0,0 +1,31 @@
+import {fetchBaseQuery} from "@reduxjs/toolkit/query";
+import {createApi} from "@reduxjs/toolkit/query/react";
+
+const API_KEY = import.meta.env.VITE_API_KEY || "U4DMV*8nvpm3EOpvf69Rxw(("
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "https://api.stackexchange.com/2.3/"
+
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: BASE_URL
+})
+
+export const api = createApi({
+    reducerPath: "api",
+    baseQuery,
+    endpoints: (builder) => ({
+        getAllTags: builder.query({
+            query: (args) => {
+                return {
+                    url: "/tags",
+                    params: {
+                        ...args,
+                        site: "stackoverflow",
+                        key: API_KEY
+                    }
+                }
+            }
+        })
+    })
+})
+
+export const {useGetAllTagsQuery} = api;
