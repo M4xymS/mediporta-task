f0ffb793d261e9bd5ea46a30f1b53cf961068a83
Maksym <smolinskimaksym@gmail.com>
2024-03-29 18:49:39 +0100

    Added loading state to table

--- src/components/table-grid/TableGrid.tsx
+++ src/components/table-grid/TableGrid.tsx
@@ -1,14 +1,16 @@
-import {Table, TableBody, TableHeader} from "@/components/ui/table.tsx";
+import {Table, TableBody, TableCell, TableHeader, TableRow} from "@/components/ui/table.tsx";
 import {Column} from "@/types";
 import TableHeaderRows from "@/components/table-grid/TableHeaderRows.tsx";
 import TableBodyRows from "@/components/table-grid/TableBodyRows.tsx";
+import {SymbolIcon} from "@radix-ui/react-icons";
 
 interface TableGridProps<T> {
     data: T[];
     headers: Column<T>[];
+    isLoading: boolean
 }
 
-const TableGrid = <T,>({ data, headers }: TableGridProps<T>) => {
+const TableGrid = <T,>({ data, headers, isLoading }: TableGridProps<T>) => {
     function formatData(columns: Column<T>[], data: T[]): Partial<T>[] {
         if (data && columns) {
             return data.map((item) => {
@@ -26,6 +28,7 @@ const TableGrid = <T,>({ data, headers }: TableGridProps<T>) => {
     }
     const formattedData: Partial<T>[] = formatData(headers, data);
 
+
     return (
         <div className='w-full space-y-2 overflow-auto'>
             <div className="rounded-md w-full border mb-4">
@@ -34,7 +37,17 @@ const TableGrid = <T,>({ data, headers }: TableGridProps<T>) => {
                         <TableHeaderRows headers={headers} />
                     </TableHeader>
                     <TableBody>
-                        <TableBodyRows formattedData={formattedData} />
+                        {isLoading ? (
+                            <TableRow>
+                                <TableCell colSpan={headers.length}>
+                                    <div className="flex h-80 items-center overflow-hidden justify-center">
+                                        <SymbolIcon className='size-16 animate-spin'/>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        ) : (
+                            <TableBodyRows formattedData={formattedData} />
+                        )}
                     </TableBody>
                 </Table>
             </div>
