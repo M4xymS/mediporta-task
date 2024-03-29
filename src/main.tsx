8b7c549b253b58a2e20165745a16f3e193868e53
Maksym <smolinskimaksym@gmail.com>
2024-03-29 18:00:18 +0100

    added tooltip provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import store from "@/store/store.ts";
 import {Provider} from "react-redux";
+import {TooltipProvider} from "@/components/ui/tooltip.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <Provider store={store}>
-    <App />
-      </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <TooltipProvider delayDuration={10} disableHoverableContent>
+                <App/>
+            </TooltipProvider>
+        </Provider>
+    </React.StrictMode>,
 )
