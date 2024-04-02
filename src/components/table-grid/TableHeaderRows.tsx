e6e000011e247217e9e9426c07319e104a31d861
Maksym <smolinskimaksym@gmail.com>
2024-04-03 00:23:29 +0200

    refactored table to work with redux

--- src/components/table-grid/TableHeaderRows.tsx
+++ src/components/table-grid/TableHeaderRows.tsx
@@ -1,24 +1,77 @@
-import {TableHead, TableRow} from "@/components/ui/table.tsx";
-import {Column} from "@/types";
+import {TableHead} from "@/components/ui/table.tsx";
+import {Column, SortingOrder} from "@/types";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {CaretDownIcon, CaretSortIcon, CaretUpIcon} from "@radix-ui/react-icons";
+import {useAppDispatch} from "@/store/storeHooks.ts";
+import {setOrder, setSort} from "@/features/tableSettings.ts";
 
 
 interface TableHeaderRowsProps<T> {
-    headers: Column<T>[]
+    header: Column<T>
 }
 
-const TableHeaderRows = <T, >({headers}: TableHeaderRowsProps<T>) => {
+const TableHeaderRows = <T, >({header}: TableHeaderRowsProps<T>) => {
+    const params = new URLSearchParams(window.location.search);
+    const dispatch = useAppDispatch();
+
+    if (!header.sort) {
+        return <TableHead>{header.header}</TableHead>
+    }
+    const handleClick = (order: SortingOrder) => {
+        dispatch(setOrder(order));
+        dispatch(setSort(header.sort));
+        params.set('order', order);
+        params.set('sort', header.sort as string);
+        window.history.replaceState({}, '', `?${params}`);
+    }
+
     return (
-        <TableRow>
-            {headers.map((header, index) => {
-                const headerSize = header?.size ? {width: `${header.size}px`} : {width: "150px"};
-                return (
-                    <TableHead style={headerSize} key={index}>
-                        {header.id ? null : header.header}
-                    </TableHead>
-                );
-            })}
-        </TableRow>
+        <TableHead>
+            <div className='flex items-center'>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className="h-8"
+                        >
+                            {header.header}
+                            {header.sort && (
+                                <CaretSortIcon className="ml-2 size-4"/>
+                            )}
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem
+                            onClick={() => handleClick('asc')}
+                            className='cursor-pointer'
+                        >
+                            <CaretUpIcon
+                                className="mr-2 size-3.5"
+                            />
+                            Asc
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={() => handleClick('desc')}
+                            className='cursor-pointer'
+                        >
+                            <CaretDownIcon
+                                className="mr-2 size-3.5"
+                            />
+                            Desc
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </TableHead>
     )
+
 }
 
 export default TableHeaderRows
