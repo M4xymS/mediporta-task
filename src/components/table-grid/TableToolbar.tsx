ea86865038e94140ed79a07b78dcc05a4eea5d41
Maksym <smolinskimaksym@gmail.com>
2024-04-02 21:13:47 +0200

    added input to contain maxPageSize

--- src/components/table-grid/TableToolbar.tsx
+++ src/components/table-grid/TableToolbar.tsx
@@ -5,7 +5,12 @@ import {
     PaginationNext,
     PaginationPrevious
 } from "@/components/ui/pagination.tsx";
-import {FC, useEffect} from "react";
+import {ChangeEvent, FC, useEffect} from "react";
+import {Input} from "@/components/ui/input.tsx";
+import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip.tsx";
+import {useAppDispatch} from "@/store/storeHooks.ts";
+import {setPageSize} from "@/features/tableSettings.ts";
+import useDebounce from "@/hooks/useDebounce.ts";
 
 interface TableToolbarProps {
     currentPage?: number
@@ -13,13 +18,14 @@ interface TableToolbarProps {
 }
 
 const TableToolbar: FC<TableToolbarProps> = ({currentPage = 1, setCurrentPage}) => {
-
+    const dispatch = useAppDispatch();
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         params.set('page', currentPage.toString());
 
         window.history.replaceState({}, '', `?${params}`);
     }, [currentPage]);
+
 
     const onNextPage = () => {
         if (setCurrentPage) {
@@ -36,18 +42,50 @@ const TableToolbar: FC<TableToolbarProps> = ({currentPage = 1, setCurrentPage}) 
         }
     }
 
+    const debouncedSetPageSize = useDebounce((value) => {
+        dispatch(setPageSize(value));
+    }, 500);
+
+    const onPageChange = (event: ChangeEvent<HTMLInputElement>) => {
+        let value = parseInt(event.target.value, 10);
+        value = isNaN(value) ? 0 : value;
+        value = Math.min(100, Math.max(0, value));
+
+        debouncedSetPageSize(value);
+    }
+
+
     if (setCurrentPage || currentPage) {
         return (
-            <Pagination className='flex justify-end'>
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious onClick={onPreviousPage} className='cursor-pointer'>Previous</PaginationPrevious>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext onClick={onNextPage} className='cursor-pointer'>Next</PaginationNext>
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+            <div className='flex justify-end'>
+                <div className='flex'>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Input
+                                className='mr-2 w-16'
+                                type="number"
+                                max={100}
+                                onChange={onPageChange}
+                            />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            Pagination
+                        </TooltipContent>
+                    </Tooltip>
+
+                    <Pagination className='flex justify-end'>
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious onClick={onPreviousPage}
+                                                    className='cursor-pointer'>Previous</PaginationPrevious>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationNext onClick={onNextPage} className='cursor-pointer'>Next</PaginationNext>
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
+            </div>
         )
     }
 }
