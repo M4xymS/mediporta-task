50b31409a0230831fb0c6fb269fb57d046d8957f
Maksym <smolinskimaksym@gmail.com>
2024-04-03 00:57:40 +0200

    clean up tablegrid

--- src/components/table-grid/TableGrid.tsx
+++ src/components/table-grid/TableGrid.tsx
@@ -1,24 +1,50 @@
 import {Table, TableBody, TableCell, TableHeader, TableRow} from "@/components/ui/table.tsx";
 import {Column} from "@/types";
+import {ExclamationTriangleIcon, SymbolIcon} from "@radix-ui/react-icons";
 import TableHeaderRows from "@/components/table-grid/TableHeaderRows.tsx";
 import TableBodyRows from "@/components/table-grid/TableBodyRows.tsx";
-import {SymbolIcon} from "@radix-ui/react-icons";
 import TableToolbar from "@/components/table-grid/TableToolbar.tsx";
 
 interface TableGridProps<T> {
     data?: T[];
     headers: Column<T>[];
-    isLoading?: boolean
-    pagination?: boolean
-    maxPageSize?: number
+    isLoading?: boolean;
+    pagination?: boolean;
+    maxPageSize?: number;
+    isError?: boolean;
+    error?: unknown;
 }
+
+const ErrorRow = ({error, headersLength}: { error: unknown, headersLength: number }) => (
+    <TableRow>
+        <TableCell colSpan={headersLength}>
+            <div className="flex h-80 items-center text-red-600 overflow-hidden justify-center">
+                <ExclamationTriangleIcon className='size-16 '/>
+                <p>{JSON.stringify(error)}</p>
+                <p>Please, contact an administrator</p>
+            </div>
+        </TableCell>
+    </TableRow>
+);
+
+const LoadingRow = ({headersLength}: { headersLength: number }) => (
+    <TableRow>
+        <TableCell colSpan={headersLength}>
+            <div className="flex h-80 items-center overflow-hidden justify-center">
+                <SymbolIcon className='size-16 animate-spin'/>
+            </div>
+        </TableCell>
+    </TableRow>
+);
 
 const TableGrid = <T, >({
                             data,
                             headers,
                             isLoading,
                             pagination = false,
-                            maxPageSize
+                            maxPageSize,
+                            isError,
+                            error
                         }: TableGridProps<T>) => {
 
     function formatData(columns: Column<T>[], data: T[] | undefined): Partial<T>[] {
@@ -52,15 +78,9 @@ const TableGrid = <T, >({
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {isLoading ? (
-                            <TableRow>
-                                <TableCell colSpan={headers.length}>
-                                    <div className="flex h-80 items-center overflow-hidden justify-center">
-                                        <SymbolIcon className='size-16 animate-spin'/>
-                                    </div>
-                                </TableCell>
-                            </TableRow>
-                        ) : (
+                        {isError && <ErrorRow headersLength={headers.length} error={error}/>}
+                        {isLoading && <LoadingRow headersLength={headers.length}/>}
+                        {!isError && formattedData && !isLoading && (
                             <TableBodyRows headers={headers} formattedData={formattedData}/>
                         )}
                     </TableBody>
