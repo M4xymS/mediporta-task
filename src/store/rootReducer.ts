d93f8001b6ff46b988ccc3a2b78c316f23514ebe
Maksym <smolinskimaksym@gmail.com>
2024-04-02 21:13:28 +0200

    added tableSettings reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,9 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {api} from "@/api/api.ts";
-
+import tableSettings from "@/features/tableSettings.ts";
 const rootReducer = combineReducers({
     [api.reducerPath]: api.reducer,
+    tableSettings
 })
 
 export default rootReducer
