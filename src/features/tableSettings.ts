5fe908bc1dd56b4b51ad7491acc0d1c564d4924e
Maksym <smolinskimaksym@gmail.com>
2024-04-03 00:22:55 +0200

    add new actions and reducers

--- src/features/tableSettings.ts
+++ src/features/tableSettings.ts
@@ -1,21 +1,51 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {RootState} from "@/store/store.ts";
+import {api} from "@/api/api.ts";
 
+
+const initialState = {
+    pageSize: 100,
+    page: Number(new URLSearchParams(window.location.search).get('page')) || 1,
+    hasMore: true,
+    order: new URLSearchParams(window.location.search).get('order') || 'desc',
+    sort: new URLSearchParams(window.location.search).get('sort') || 'popular'
+}
 
 const tableSettings = createSlice({
     name: "tableSettings",
-    initialState: {
-        pageSize: 10,
-    },
+    initialState,
     reducers: {
         setPageSize(state, action) {
             state.pageSize = action.payload;
+        },
+        setPage(state, action) {
+            state.page = action.payload;
+        },
+        setSort(state, action) {
+            state.sort = action.payload;
+        },
+        setOrder(state, action) {
+            state.order = action.payload;
         }
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(api.endpoints?.getAllTags.matchFulfilled, (state, action) => {
+            state.hasMore = action.payload.has_more;
+        })
     }
 });
 
-export const {setPageSize} = tableSettings.actions;
+export const {setPageSize, setPage, setSort, setOrder} = tableSettings.actions;
 
 
-export const getPageSize = (state) => state.tableSettings.pageSize;
+export const getPageSize = (state: RootState) => state.tableSettings.pageSize;
+
+export const getPage = (state: RootState) => state.tableSettings.page;
+
+export const getSort = (state: RootState) => state.tableSettings.sort;
+
+export const getOrder = (state: RootState) => state.tableSettings.order;
+
+export const getHasMore = (state: RootState) => state.tableSettings.hasMore;
 
 export default tableSettings.reducer;
